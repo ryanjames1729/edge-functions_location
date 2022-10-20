e24cece6e2ea526ae222dd2ea1d6f5b024a98c7e
Ryan James <rjames@rjames-mbpr-it.cdschool.org>
2022-10-20 12:25:00 -0400

    added edge function

---
+++ netlify/edge-functions/transform-hello-template.js
@@ -0,0 +1,28 @@
+// netlify/edge-functions/transform-hello-template.js
+
+export default async (request, context) => {
+    const url = new URL(request.url);
+   
+    // Look for the "?method=transform" query parameter
+    // return if we don't find it
+    if (url.searchParams.get("method") !== "transform") {
+      return;
+    }
+   
+    // Get the page content that will be served next
+    // In this tutorial, it will be the content from hello-template
+    const response = await context.next();
+    const page = await response.text();
+   
+    // Search for the placeholder
+    const regex = /LOCATION_UNKNOWN/i;
+   
+    // Get the location from the Context object
+    const location = `${context.geo.city}, ${context.geo.country.name}`;
+   
+    // Replace the content with the current location
+    const updatedPage = page.replace(regex, location);
+   
+    // Return the response
+    return new Response(updatedPage, response);
+   };
